66f67e2e8b7aced0c8f3f4c1adb981d57637d2d3
justtoconfirm <office@justtoconfirm.co.uk>
2021-05-13 20:15:45 +0100

    Setup PeerJS to generate dynamic user ID

--- public/script.js
+++ public/script.js
@@ -1,8 +1,15 @@
 // Socket.io to connect to our root path
 const socket = io('/')
+// Peer will generate the id of the user
+const myPeer = new Peer(undefined, {
+	host: '/',
+	port: '3001'
+})
 
-// Send event to our server and pass in the id of room and user
-socket.emit('join-room', ROOM_ID, 10)
+myPeer.on('open', id => {
+	// Send event to our server and pass in the id of room and user
+	socket.emit('join-room', ROOM_ID, id)
+})
 
 socket.on('user-connected', userId => {
 	console.log('User connected: ' + userId)
