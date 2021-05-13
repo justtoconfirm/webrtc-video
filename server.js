ee2dbc9e84baa79f65d0af25935cc2183103ac25
justtoconfirm <office@justtoconfirm.co.uk>
2021-05-13 19:26:01 +0100

    Setup routes and rooms with dynamic URLs generated from uuid within the server.js file

--- server.js
+++ server.js
@@ -4,6 +4,27 @@ const app = express()
 const server = require('http').Server(app)
 // Pass the server to Socket.io to know what server is being used
 const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+// Setup the route
+app.set('view engine', 'ejs')
+// Setup the static folder containing our JavaScript and CSS code
+app.use(express.static('public'))
+
+// Home route
+app.get('/', (req, res) => {
+	/*
+		Redirect users to the correct room and provide a dynamic URL using uuid
+		This dynamic URL will be generated each time the user goes to the homepage
+	*/
+	res.redirect(`/${uuidV4()}`)
+})
+
+// Create rooms for our users with dynamic parameter
+app.get('/:room', (req, res) => {
+	// Room value comes from the URL parameter
+	res.render('room', { roomId: req.params.room })
+})
 
 // Start server and listen on the specified port
 server.listen(3000)
